2e7650093aa2e67f240b50e6a739b13a92ee8929
Jess Todd <jessstodd@gmail.com>
2025-02-10 20:58:45 +0000

    added pages

---
+++ src/app/code/page.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export default function Page() {
+    return <main className={styles.blog}>
+      <div className={styles.container}>
+        <h1>code</h1>
+      </div>
+    </main>
+}
---
+++ src/app/connect/page.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export default function Page() {
+    return <main className={styles.blog}>
+      <div className={styles.container}>
+        <h1>connect</h1>
+      </div>
+    </main>
+}
