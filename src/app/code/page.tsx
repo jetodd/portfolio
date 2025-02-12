09b6a569a5cd65ec33108d3adb38dbbb11dd6ff2
Jess Todd <jessstodd@gmail.com>
2025-02-12 22:46:15 +0000

    added base pages for code and connect

--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -1,9 +1,10 @@
 import styles from './styles.module.css'
 
 export default function Page() {
-    return <main className={styles.blog}>
-      <div className={styles.container}>
+    return <main className={styles.container}>
+      <section className="mx-auto -my-32 md:my-0 flex h-screen w-screen flex-col justify-center">
         <h1>code</h1>
-      </div>
+        
+      </section>
     </main>
 }
