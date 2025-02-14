a32b3802fefc0c498bea9e451992fa92c7697e41
Jess Todd <jessstodd@gmail.com>
2025-02-14 15:35:52 +0000

    remove not needed css files

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,10 +1,9 @@
-import styles from './styles.module.css'
-
 export default function Page() {
-    return <main className={styles.container}>
+    return <main 
+      className="h-dvh w-dvh justify-center content-center text-center p-4" 
+      style={{ ["backgroundColor" as any]: 'var(--color-contact)' }}>
       <section className="mx-auto -my-32 md:my-0 flex h-screen w-screen flex-col justify-center">
-        <h1>connect</h1>
-        
+        <span className='text-2xl'>contact</span>
       </section>
     </main>
 }
