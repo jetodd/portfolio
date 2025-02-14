81e5fec944974a526f5f49e378f614d61fcf45e2
Jess Todd <jessstodd@gmail.com>
2025-02-14 15:37:53 +0000

    removed padding causing scrolling

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
     return <main 
-      className="h-dvh w-dvh justify-center content-center text-center p-4" 
+      className="h-dvh w-dvh justify-center content-center text-center" 
       style={{ ["backgroundColor" as any]: 'var(--color-contact)' }}>
       <section className="mx-auto -my-32 md:my-0 flex h-screen w-screen flex-col justify-center">
         <span className='text-2xl'>contact</span>
