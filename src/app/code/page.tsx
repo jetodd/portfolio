793cb2f161d2ecabe89423daf9f8bb494004d23b
Jess Todd <jessstodd@gmail.com>
2025-02-14 15:29:51 +0000

    use tailwind for css

--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -1,7 +1,9 @@
 import styles from './styles.module.css'
 
 export default function Page() {
-    return <main className={styles.container}>
+    return <main 
+      className="h-dvh w-dvh justify-center content-center text-center p-4" 
+      style={{ ["backgroundColor" as any]: 'var(--color-code)' }}>
       <section className="mx-auto -my-32 md:my-0 flex h-screen w-screen flex-col justify-center">
         <h1>code</h1>
         
