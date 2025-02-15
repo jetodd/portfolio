a706c957947b7ebd86c5ca881cd7f17813ed8a5a
Jess Todd <jessstodd@gmail.com>
2025-02-15 12:57:04 +0000

    Tweak panel for responsiveness
    See #2

--- src/app/components/Panel/Panel.module.tsx
+++ src/app/components/Panel/Panel.module.tsx
@@ -24,7 +24,7 @@ export default function Panel({
       ref={ref}
       className={
         "z-[calc(250)] absolute h-full right-0 top-0 text-white " +
-        (isComponentVisible ? "md:w-3/4 w-5/6" : "w-0") +
+        (isComponentVisible ? "w-full md:w-5/6 lg:w-3/4" : "w-0") +
         " max-w-xl transition-all duration-1000 ease-in-out transform h-fill"
       }
       style={{ ["backgroundColor" as any]: cosplay?.bgColor }}
