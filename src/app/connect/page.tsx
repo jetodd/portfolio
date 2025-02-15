344339c9c797fced3e70c6e4be231f8edc9e5f04
Jess Todd <jessstodd@gmail.com>
2025-02-15 11:46:45 +0000

    show svg on hover

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <div className="grid grid-cols-1 md:grid-cols-4 mx-8 mx-auto max-w-[200px] md:max-w-5xl md:min-h-[160px]">
           <div className="py-1 text-black rounded-t-full md:rounded-tr-none md:rounded-l-full content-center justify-center"
             style={{ ["backgroundColor" as any]: 'var(--color-pink)' }}>
-            <a href="https://www.linkedin.com/in/jess-todd-76b75b90/" target="_blank">
+            <a href="https://www.linkedin.com/in/jess-todd-76b75b90/" className={styles.link} target="_blank">
               <span className="grid place-content-center">
                 <ContactItem icon='linked-in'></ContactItem>
               </span>
@@ -18,7 +18,7 @@ export default function Page() {
           </div>
           <div className="bg-white py-1 text-black content-center display-grid"
             style={{ ["backgroundColor" as any]: 'var(--color-pink)' }}>
-            <a href="https://github.com/jetodd" target="_blank">
+            <a href="https://github.com/jetodd" className={styles.link} target="_blank">
               <span className="grid place-content-center">
                 <ContactItem icon='github'></ContactItem>
               </span>
@@ -27,7 +27,7 @@ export default function Page() {
           </div>
           <div className="bg-white py-1 text-black content-center"
             style={{ ["backgroundColor" as any]: 'var(--color-pink)' }}>
-            <a href="mailto:jessstodd@gmail.com">
+            <a href="mailto:jessstodd@gmail.com" className={styles.link}>
               <span className="grid place-content-center">
                 <ContactItem icon='email'></ContactItem>
               </span>
