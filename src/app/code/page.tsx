eedc66111a4876dc73f1bfa6a7a81a6f149560b5
Jess Todd <jessstodd@gmail.com>
2025-02-16 19:45:00 +0000

    Iterate design

--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -22,6 +22,7 @@ export default function Page() {
           description="The site that you are currently viewing. It is a showcase of my work and a playground for me to experiment with new technologies."
           icon={<Portfolio />}
           techStack={["Next.js", "TypeScript", "Tailwind CSS"]}
+          siteUrl="/"
         />
         <CodeItem
           title="Dicey"
@@ -29,6 +30,7 @@ export default function Page() {
           description="A fun little dice rolling app that I made for when people forget Dice and want to play RPG games."
           icon={<Dice />}
           techStack={["Next.js", "TypeScript", "Tailwind CSS"]}
+          siteUrl="https://github.com/jetodd/dicey"
           
         />
         <CodeItem
@@ -37,6 +39,7 @@ export default function Page() {
           description="Keeps track of all the recipes that I like, what I'm cooking this week and what needs buying at the supermarket."
           icon={<Recipes />}
           techStack={["Django", "Python", "Tailwind CSS", "PostgreSQL"]}
+          siteUrl="https://github.com/jetodd/recipes"
         />
       </section>
     </main>
--- src/app/components/CodeItem/CodeItem.tsx
+++ src/app/components/CodeItem/CodeItem.tsx
@@ -2,26 +2,62 @@ import { JSX } from "react";
 import GitHub from "../Icons/GitHub";
 
 export type CodeItemProps = {
-    title: string
-    icon: JSX.Element
-    githubRepo: string
-    techStack: string[]
-    description: string
-}
+  title: string;
+  icon: JSX.Element;
+  githubRepo: string;
+  techStack: string[];
+  description: string;
+  siteUrl: string;
+};
 
-export default function CodeItem({title, icon, githubRepo, description}: CodeItemProps) {
+export default function CodeItem({
+  title,
+  icon,
+  githubRepo,
+  siteUrl,
+  techStack,
+  description,
+}: CodeItemProps) {
   return (
-    <div className="max-w-sm p-6 bg-sky-900 border rounded-lg shadow-xs border-gray-700 mb-1">
-      <div className={"w-12"}>{icon}</div>
-      <a href="#">
-        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-          {title}
-        </h5>
-      </a>
-      <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+    <div className="max-w-sm p-6 bg-sky-800 border rounded-lg shadow-xs border-gray-700 mb-1">
+      <p className="mb-3 font-normal text-gray-450">
         {description}
       </p>
-        <a className="w-12 block" title="View on GitHub" target="_blank" href={`https://github.com/jetodd/${githubRepo}`}><GitHub /></a>
+      <div className="flex mb-3 gap-3">
+        <a
+          className="w-7"
+          title="View on GitHub"
+          target="_blank"
+          href={`https://github.com/jetodd/${githubRepo}`}
+        >
+          <GitHub />
+        </a>
+        <a
+          className="w-14 text-center align-middle text-xs bg-sky-900 hover:bg-sky-950 text-gray-350 rounded-full px-2 py-1 mr-1"
+          target="_blank"
+          href={siteUrl}
+        >
+          Site
+        </a>
+      </div>
+      <div className="flex mb-3">
+        <a href="#">
+          <h2 className="mb-2 text-3xl rounded-md border-1 border-sky-950 px-2 bg-sky-800 relative -left-15 font-semibold tracking-tight dark:text-white">
+            {title}
+          </h2>
+        </a>
+        <div className={"w-12 -ml-10"}>{icon}</div>
+      </div>
+      <div className="flex">
+        {techStack.map((tech) => (
+          <span
+            key={tech}
+            className="text-xs bg-sky-900 text-gray-350 rounded-full px-2 py-1 mr-1"
+          >
+            {tech}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
